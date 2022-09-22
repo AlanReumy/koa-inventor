9a7d745d6d33d14a7031f02427a97c268ebd211b
vikoala <vikoala@foxmail.com>
2022-09-22 20:21:52 +0800

    fix: package.json path

--- src/util/programUtil.ts
+++ src/util/programUtil.ts
@@ -2,13 +2,12 @@ import path from "path";
 import fs from "fs";
 import { Command } from "commander";
 import { fileURLToPath } from "url";
-import process from "process";
 import { ProgramCommand, ProgramInfo } from "../type/program";
 
 export function getPackageJson() {
   const __dirname = fileURLToPath(import.meta.url);
   return JSON.parse(
-    fs.readFileSync(path.resolve(process.cwd(), "./package.json"), {
+    fs.readFileSync(path.resolve(__dirname, "../../package.json"), {
       encoding: "utf-8",
     })
   );
